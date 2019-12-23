f9f7592aa5f8e22da8772513642b95fb0320dd04
andysml <andy922200@gmail.com>
2019-12-23 16:32:44 +0800

    move file to server folder

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-let apis = require('./apis')
-
-module.exports = (app) => {
-  app.use('/api', apis)
-  app.get('*', (req, res) => {
-    res.status(404).send({ error: 'Please check your request again.' })
-  })
-}
---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+const apis = require('./apis')
+
+module.exports = (app) => {
+  app.use('/api', apis)
+  app.get('*', (req, res) => {
+    res.status(404).send({ error: 'Please check your request again.' })
+  })
+}
