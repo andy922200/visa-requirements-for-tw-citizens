aaa57abcffc3304ad856883cdfed466b536a914b
andysml <andy922200@gmail.com>
2019-12-23 17:21:09 +0800

    init frontend layout

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default {
+  name: "App"
+};
+</script>
+
+
---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,11 @@
+// ./src/views/NotFound.vue
+<template>
+  <div>
+    <h2>404 Error</h2>
+    <p>ddd</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
