2f7f822399a1787fcb8f35e699b3dd3e168da965
andysml <andy922200@gmail.com>
2019-12-23 15:14:43 +0800

    finish countryController.js

--- controllers/api/countryController.js
+++ controllers/api/countryController.js
@@ -3,18 +3,18 @@ const Category = db.Category
 const Continent = db.Continent
 const Region = db.Region
 const Country = db.Country
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+const path = require('path')
 const pageLimit = 10
 
 let countryController = {
-  getCountries: (req, res) => {
+  getAllCountries: (req, res) => {
     Country.findAndCountAll({
       include: [Category, Continent, Region],
       order: ['id']
     })
       .then(countries => {
-        //let page = Number(req.query.page) || 1
-        //let prev = page - 1 < 1 ? 1 : page - 1
-        //let next = page + 1 > pages ? pages : page + 1
         let allCountsInDB = countries.count
         let allPages = Math.ceil(allCountsInDB / pageLimit)
         let pages = Math.ceil(countries.rows.length / pageLimit)
@@ -37,14 +37,75 @@ let countryController = {
         return res.json({
           allCountsInDB: allCountsInDB,
           allPages: allPages,
+          originData: temp,
           pageLimit: pageLimit,
           countries: listWithPages,
           totalPage: totalPage,
-          //page: page,
-          //prev: prev,
-          //next: next
         })
       })
+  },
+  getCountry: (req, res) => {
+    let countryName = path.basename(req.path)
+    Country.findOne({
+      include: [Category, Continent, Region],
+      order: ['id'],
+      where: {
+        [Op.or]: {
+          name:
+            { [Op.like]: '%' + countryName + '%' },
+          nameEn:
+            { [Op.like]: '%' + countryName + '%' }
+        }
+      }
+    }).then(country => {
+      return res.json({
+        country: country
+      })
+    })
+  },
+  getMultipleCountries: (req, res) => {
+    let query = req.query.q.split(',')
+    query = query.map(item => {
+      return { [Op.like]: '%' + item + '%' }
+    })
+
+    Country.findAndCountAll({
+      include: [Category, Continent, Region],
+      order: ['id'],
+      where: {
+        [Op.or]: {
+          name: { [Op.or]: query },
+          nameEn: {
+            [Op.or]: query
+          }
+        }
+      }
+    }).then(countries => {
+      let allCounts = countries.count
+      let pages = Math.ceil(allCounts / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let listWithPages = new Object()
+      let temp = countries.rows.map(country => ({
+        ...country.dataValues
+      }))
+
+      for (let i = 0; i < pages; i++) {
+        listWithPages[i + 1] = new Array()
+        while (listWithPages[i + 1].length < pageLimit) {
+          for (let j = pageLimit * i; j <= pageLimit * i + 9; j++) {
+            listWithPages[i + 1].push(temp[j])
+          }
+        }
+        listWithPages[i + 1] = listWithPages[i + 1].filter(d => d) // 過濾掉 null
+      }
+
+      return res.json({
+        allCounts: allCounts,
+        originData: temp,
+        countries: listWithPages,
+        totalPage: totalPage
+      })
+    })
   }
 }
 
--- routes/apis.js
+++ routes/apis.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 const countryController = require('../controllers/api/countryController')
 
-router.get('/visa/countries', countryController.getCountries)
+router.get('/visa/countries', countryController.getAllCountries)
+router.get('/visa/countries/search', countryController.getMultipleCountries)
+router.get('/visa/countries/*', countryController.getCountry)
 
 module.exports = router
