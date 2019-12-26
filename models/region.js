bd56c0491654456fb53b0dbc551e7bebe4aa18cf
andysml <andy922200@gmail.com>
2019-12-26 16:31:52 +0800

    fix region model error

--- models/region.js
+++ models/region.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Region = sequelize.define('Region', {
     name: DataTypes.STRING,
     nameEn: DataTypes.STRING,
-    continentId: DataTypes.INTEGER
+    ContinentId: DataTypes.INTEGER
   }, {});
   Region.associate = function (models) {
     Region.belongsTo(models.Continent)
