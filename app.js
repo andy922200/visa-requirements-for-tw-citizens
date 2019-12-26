8bb8ec5cf1231fc8ee017d3b6e768ac1ad604eca
andysml <andy922200@gmail.com>
2019-12-26 19:41:35 +0800

    fix route-8

--- app.js
+++ app.js
@@ -33,15 +33,15 @@ app.use(
   res.locals.user = req.user
 })*/
 
+// 連結後端 api 路由
+require('./routes/')(app)
+
 // production mode 前端路由
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   // static folder
   app.use(express.static(__dirname + '/server/public/'))
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/server/public/index.html'))
 }
-// 連結後端 api 路由
-require('./routes/')(app)
-
 
 app.listen(port, () => {
   console.log(`The app is listening on port ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const apis = require('./apis')
 
 module.exports = (app) => {
   app.use('/api', apis)
-  app.get('*', (req, res) => {
-    res.status(404).send({ error: 'Please check your request again.' })
-  })
+  //以下為測試用途，佈署請刪除 
+  //app.get('*', (req, res) => res.status(404).send('Not Found'))
 }
