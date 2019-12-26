0b2bf2080687cbfc7e70b8fcf40b75d18759a33f
andysml <andy922200@gmail.com>
2019-12-26 21:14:28 +0800

    fix route-10

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,5 @@ const apis = require('./apis')
 
 module.exports = (app) => {
   app.use('/api', apis)
-  app.get('*', (req, res) => res.status(404).send('Not Found'))
+  //app.get('*', (req, res) => res.status(404).send('Not Found'))
 }
