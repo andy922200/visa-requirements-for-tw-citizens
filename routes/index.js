a13636cb5c34bb458a9c80c2b53572d326272ee5
andysml <andy922200@gmail.com>
2019-12-26 20:49:02 +0800

    test add server folder

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const apis = require('./apis')
-
-module.exports = (app) => {
-  app.use('/api', apis)
-  //以下為測試用途，佈署請刪除 
-  //app.get('*', (req, res) => res.status(404).send('Not Found'))
-}
---
+++ server/routes/index.js
@@ -0,0 +1,6 @@
+const apis = require('./apis')
+
+module.exports = (app) => {
+  app.use('/api', apis)
+  app.get('*', (req, res) => res.status(404).send('Not Found'))
+}
